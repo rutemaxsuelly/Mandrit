56cecb86b2feef3a0963bdad1848004dce70f177
Rute Maxsuelly <rutymaxsuelly@gmail.com>
2021-02-20 01:44:28 +0000

    Updates
    Ajustes
    API Mandrit: Extrator de dados incluindo parâmetros:
    1. Janela de Analise
    2.Granularidade do tempo de deltatime(ticks) em segundos
    3. Organização de pastas por fórmulas do compasso
    4. Melhorias na referência do compasso simplificada (2/4, 3/4, 4/4, 5/4)
    5.Novas gerações de visualizações musicais
    6.Iniciando Implementação de tutorial
    7.Tentativa de implementação do cursor animado

--- MandRit/midicsv/midi.py
+++ MandRit/midicsv/midi.py
@@ -2,9 +2,9 @@ import py_midicsv
 
 # Load the MIDI file and parse it into CSV format
 folder = "C:\\Users\\rutem\\Documents\\GitHub\\Music_Visual\\MandRit\\midicsv\\"
-filename = "midi_file\\Base_MandRit\\_Plotar_Midis\\bateria04.mid"
+filename = "midi_file\\Base_MandRit\\_Plotar_Midis\\bateri.mid"
 csv_string = py_midicsv.midi_to_csv(folder + filename)
-nome_do_csv = folder + "csv\\File_tracksname\\bateria04_BUG.csv"
+nome_do_csv = folder + "csv\\File_tracksname\\bateriTEMPO_segundos.csv"
 
 def get_compasso(csv_string):
     for line in csv_string:
@@ -30,39 +30,57 @@ def get_time_signature(csv_string):
 # Parse the CSV output of the previous command back into a MIDI file
 #midi_object = py_midicsv.csv_to_midi(csv_string)
 
-def csv_formater(line, division, numerador, denominador):
+def csv_formater(line, ticksPerBeat, numerador, denominador):
     #tempo;note;velocity;channel;compasso
     col = line.split(",")
     event = col[2]
     if(event == " Note_on_c"):
         track = int(col[0])
         nota = col[-2]
-        clocks_por_compasso = division*4*numerador/2**denominador
-        print(clocks_por_compasso)
+        #Parametro para o usuário 
+        tamanhoAnalise = 4
+        #/ 5/ 6/ 2/ 0.5
+        tiques_por_compasso = ticksPerBeat*4*numerador/2**denominador 
+        janelaAnalise = tamanhoAnalise*tiques_por_compasso
+        print(janelaAnalise)
+        print(ticksPerBeat)
         
-        tempo = int(col[1].replace(" "," "))
-        sub_divisao_compasso = tempo%clocks_por_compasso
-        print(tempo)
-        if(track == 2):
-            print(track, nota, division, numerador, denominador, clocks_por_compasso, tempo, sub_divisao_compasso)
-        return track, sub_divisao_compasso, nota
+        print(tiques_por_compasso)
+        #tempo = deltatime registrados na sequência MIDI
+        tempoDelta = int(col[1].replace(" "," "))
+        sub_divisao_compasso_tiques= tempoDelta%janelaAnalise
+
+        # primeira tentativa calcular tempo em seg
+        #MICROSECOND_PER_MINUTES = 60000000
+        #tempoSegundos = tempoDelta *(60/ MICROSECOND_PER_MINUTES* ticksperbeat)
+        # sub_divisao_compasso= tempoSegundos%janelaAnalise
+        # ----------------------------------------------------------------#
+        #ou -> 
+
+        #Para músicas 120 BPM
+        tempobpm = 120
+        microsecondsPerMinute = 1000000*60 / tempobpm
+        sub_divisao_compasso_segundos = sub_divisao_compasso_tiques*( 60 / microsecondsPerMinute/tempobpm)* ticksPerBeat
+        print(sub_divisao_compasso_segundos)
+        return track, sub_divisao_compasso_segundos, nota
     else:
         return None, None, None
 
+
 # Save the parsed MIDI file to disk
 def separate_note_por_compasso(csv_string):
-    division = get_compasso(csv_string)
+    ticksPerBeat = get_compasso(csv_string)
     numerador, denominador = get_time_signature(csv_string)
     dic_matrix = {}
     for line in csv_string:
         print(line)
-        track, sub_divisao_compasso, nota = csv_formater(line, division, numerador, denominador)
+        track, sub_divisao_compasso_segundos, nota = csv_formater(line, ticksPerBeat, numerador, denominador)
         if(track == None):
             continue
-        if ((track, sub_divisao_compasso) in dic_matrix.keys()):
-            dic_matrix[(track, sub_divisao_compasso)].append(nota)
+        if ((track, sub_divisao_compasso_segundos) in dic_matrix.keys()):
+            dic_matrix[(track, sub_divisao_compasso_segundos)].append(nota)
         else:
-            dic_matrix[(track, sub_divisao_compasso)] = [nota]
+            dic_matrix[(track, sub_divisao_compasso_segundos)] = [nota]
     
     return dic_matrix
 
